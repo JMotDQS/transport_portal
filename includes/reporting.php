8e108c76b9e0e4f9c968ee8aa24c92f13a8d7c5a
JMotDQS <147440538+JMotDQS@users.noreply.github.com>
2024-09-23 15:51:19 -0400

    Update reporting.php

--- includes/reporting.php
+++ includes/reporting.php
@@ -3,6 +3,12 @@
 	require_once("config.php");
 
 	$return_array = [];
+	$tracking_array = [];
+	$his_tracking_array = [];
+
+	$trackingReport = 'asset_tracking.csv';
+	$hisTrackingReport = 'historical_asset_tracking.csv';
+    $fp = fopen('php://output', 'w');
 	$serverName = $host."\\sqlexpress";
 
 	// Since UID and PWD are not specified in the $connectionInfo array,
@@ -20,16 +26,34 @@
 				WHERE at.cur_location LIKE 'MB%'
 				ORDER BY at.cur_location";
 		$res = sqlsrv_query($conn, $sql);
+
 		if (sqlsrv_has_rows($res)) {
+			$fp = fopen('../../'.$trackingReport, 'w');
+			while ($row = sqlsrv_fetch_array($res, SQLSRV_FETCH_ASSOC)) {
+				array_push($tracking_array, $row);
+				if (!isset($headings)) {
+					$headings = array_keys($row);
+					fputcsv($fp, $headings, ',', '"');
+				}
+			}
+
+			foreach ($tracking_array as $pair) {
+				fputcsv($fp, $pair);
+			}
+			fclose($fp);
+		}
+
+
+		/*if (sqlsrv_has_rows($res)) {
 			while ($row = sqlsrv_fetch_array($res, SQLSRV_FETCH_ASSOC)) {
 				array_push($return_array, $row);
 			}
-		}
+		}*/
 	}
 
-	$close_success = sqlsrv_close($conn);
+	/*$close_success = sqlsrv_close($conn);
 	if ($close_success) {
 		echo json_encode($return_array);
-	}
+	}*/
 
 ?>
