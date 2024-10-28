de938b0929ee34973bf12c72da013dd997de224a
JMotDQS <147440538+JMotDQS@users.noreply.github.com>
2024-10-28 10:01:39 -0400

    Added dialog element, css, and JavaScript

--- index.php
+++ index.php
@@ -25,7 +25,41 @@
 		<div id="app"></div>
 		<div class="search-results"></div>
 
-		<dialog id="modal" class="g_dialog"></dialog>
+		<!-- <dialog id="modal" class="g_dialog"></dialog> -->
+		<dialog  id="dialog" class="g_dialog">
+		<header class="dialog-header">
+			<div id="title">
+				<h3>Add User</h3>
+				<p>all fields required</p>
+			</div>
+			<div class="dialog-save invisible" onclick="addUser()">
+				<i class="far fa-save"></i>
+			</div>
+			<div class="dialog-close" onclick="closeModal()">
+				<i class="far fa-window-close"></i>
+			</div>
+		</header>
+		<div class="dialog-grid">
+			<div class="dialog-form-element">
+				<!--<label for="first_name">First Name:</label>-->
+				<input type="text" id="first_name" name="first_name" placeholder="First Name">
+			</div>
+			<div class="dialog-form-element">
+				<!--<label for="last_name">Last Name:</label>-->
+				<input type="text" id="last_name" name="last_name" placeholder="Last Name">
+			</div>
+			<div class="dialog-form-element">
+				<!--<label for="role" style="display:block;">Role:</label>-->
+				<select name="role" id="role">
+					<option selected="true" disabled="disabled" value="">Please Choose Role</option>	
+					<option value="Security Guard">Security Guard</option>
+					<option value="Security Supervisor">Security Supervisor</option>
+					<option value="Admin" disabled="disabled">Admin</option>
+					<option value="Driveaway">Driveaway</option>
+				</select>
+			</div>
+		</div>
+	</dialog>
 
 	<!-- JQuery CDN -->
 		<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
--- js/index.js
+++ js/index.js
@@ -4,6 +4,9 @@ $(document).ready(function() {
 		loadPage('nav', g_NAV);
 	});
 
+	console.log("g_DIALOG:", g_DIALOG);
+	g_DIALOG.showModal();
+
 	getCompaniesPromise().then(function(resolve) {
 		console.log("getCompaniesPromise:Success");
 		loadPage('nav', g_NAV);
