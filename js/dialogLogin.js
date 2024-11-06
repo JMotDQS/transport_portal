a81727d734febd9e5dc026f443751f1d7dee8d67
JMotDQS <147440538+JMotDQS@users.noreply.github.com>
2024-11-06 09:12:52 -0500

    Update dialogLogin.js

--- js/dialogLogin.js
+++ js/dialogLogin.js
@@ -10,11 +10,15 @@ function userLoginCheck(e) {
 			feedBackColoring(document.getElementById('dialog-login-error'), 'red');
 			document.getElementById('dialog-login-error').classList.add('dialog-login-error-show');
 		} else {
-			if(parseInt(resolve[0]['is_admin']) === 1) {
+			if(parseInt(resolve[0]['is_admin']) === 1 && parseInt(resolve[0]['is_active']) === 1) {
 				document.getElementById('dialog-login-error').classList.remove('dialog-login-error-show');
 				feedBackColoring(document.getElementById('dialog-login-error'));
 				document.getElementById('dialog-login-error').textContent = '';
 				g_CURRENT_LOGIN_USER_ID = parseInt(resolve[0]['pk_id']);
+
+				if(parseInt(resolve[0]['change_password']) === 1) {
+					//path for instant passwod change/update.
+				}
 
 				getCompaniesPromise().then(function(resolve) {
 					console.log("getCompaniesPromise:Success");
@@ -26,7 +30,7 @@ function userLoginCheck(e) {
 				});
 				closeDialogLogin();
 			} else {
-				document.getElementById('dialog-login-error').textContent = 'You are NOT an Admin';
+				document.getElementById('dialog-login-error').textContent = 'You are NOT an Admin and/or Active';
 				feedBackColoring(document.getElementById('dialog-login-error'), 'red');
 				document.getElementById('dialog-login-error').classList.add('dialog-login-error-show');
 			}
